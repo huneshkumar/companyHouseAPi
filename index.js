e0e92da20266271e931300ed0a2e260d9b7a32da
hunesh <huneshk@gmail.com>
2021-12-06 16:12:38 +0500

    v2

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(session({
 }));
 
 app.post('/', async (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   const { item } = req.body;
   console.log(item)
   ch.search(item).then(({ body }) => {
